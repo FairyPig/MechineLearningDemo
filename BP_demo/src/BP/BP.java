484de9f4b699ad4cb19921bc5c53a40c07a79dd6
unknown <>
2016-05-11 22:06:32 +0800

    BP-增加注释

--- BP_demo/src/BP/BP.java
+++ BP_demo/src/BP/BP.java
@@ -9,55 +9,58 @@ public class BP {
 	//输入、隐藏、输出层
 	public double[] input;
 	public double[] hidden;
+	//output为本神经网络计算出的输出值
 	public double[] output;
-	
-	//用户输出真实值
+
+	//realoutput为训练网络时，用户提供的真的输出值
 	public double[] realoutput;
-	
+
 	//v[i,j]表示输入层i到隐层j  w[i,j]表示隐层i到输出层j
 	public double[][] v;
 	public double[][] w;
-	
+
 	//beta为隐层的阈值，afa为输出层阈值
 	public double[] beta;
 	public double[] afa;
-	
+
 	//学习率
 	public double eta;
 	//步长
 	public double momentum;
 	public final Random random;
-	
+
 	public BP(int inputnum,int hiddennum,int outputnum,double learningrate){
 		innum = inputnum;
 		this.hiddennum = hiddennum;
 		outnum = outputnum;
-		
+
 		input = new double[inputnum + 1];
 		hidden = new double[hiddennum + 1];
 		output = new double[outputnum + 1];
 		realoutput = new double[outputnum + 1];
-		
+
 		v = new double[inputnum + 1][hiddennum + 1];
 		w = new double[hiddennum + 1][outputnum + 1];
-		
+
 		beta = new double[outputnum + 1];
 		afa = new double[hiddennum + 1];
 		for(int i=0;i<outputnum;i++)
 			beta[i] = 0.0;
 		for(int i=0;i<hiddennum;i++)
 			afa[i] = 0.0;
-		
+
 		eta = learningrate;
+		//随机数对结果影响较大
 		random = new Random(19950326);
-		randomizeWeights(w);  
+		randomizeWeights(w);
         randomizeWeights(v);
 	}
-	
+
 	public void testData(double[] in){
 		input = in;
 		getNetOutput();
 	}
+	//只对本题目有用，output>0.5时为好西瓜，output<0.5时为坏西瓜
 	public int predict(double[] in){
 		testData(in);
 		if(output[0]>0.5)
@@ -65,6 +68,7 @@ public class BP {
 		else
 			return 0;
 	}
+	//获得在test集上的正确率
 	public double getAccuracy(double[][] in,double[][] out){
 		int rightans = 0,wrongans = 0;
 		for(int i=0;i<in.length;i++){
@@ -79,6 +83,7 @@ public class BP {
 		System.out.println("对："+rightans+" 错："+wrongans);
 		return (double)rightans/(double)(rightans+wrongans);
 	}
+	//times为进行几轮训练
 	public void train(int times){
 		TrainData t = new TrainData();
 		double wu = 0.0,acc = 0.0;
@@ -95,12 +100,14 @@ public class BP {
 			System.out.println("预测正确率为： "+acc);
 		}
 	}
+	//对一个input输入进行训练
 	public void traindata(double[] in,double[] out){
 		input = in;
 		realoutput = out;
 		getNetOutput();
 		adjustParameter();
 	}
+	//获得误差E
 	public double getDeviation(){
 		double e = 0.0;
 		for(int i=0;i<outnum;i++)
@@ -108,6 +115,7 @@ public class BP {
 		e *= 0.5;
 		return e;
 	}
+	//调整权值
 	public void adjustParameter(){
 		double g[],e = 0.0;
 		g = new double[outnum];
@@ -129,6 +137,7 @@ public class BP {
 				v[j][i] += eta * e * input[j];
 		}
 	}
+	//获得output
 	public void getNetOutput(){
 		int i,j;
 		double tmp=0.0;
@@ -145,12 +154,12 @@ public class BP {
 			output[i] = sigmoid(tmp-beta[i]);
 		}
 	}
-	
-	private void randomizeWeights(double[][] matrix) {  
-        for (int i = 0, len = matrix.length; i != len; i++)  
-            for (int j = 0, len2 = matrix[i].length; j != len2; j++) {  
-                double real = random.nextDouble();  
-                matrix[i][j] = random.nextDouble() > 0.5 ? real : -real;  
+	//对权值矩阵w、v进行初始随机化
+	private void randomizeWeights(double[][] matrix) {
+        for (int i = 0, len = matrix.length; i != len; i++)
+            for (int j = 0, len2 = matrix[i].length; j != len2; j++) {
+                double real = random.nextDouble();
+                matrix[i][j] = random.nextDouble() > 0.5 ? real : -real;
             }
     }
 	public void debug(){
@@ -173,7 +182,7 @@ public class BP {
 		s = 1d/(1d + Math.exp(-z));
 		return s;
 	}
-	
+
 	public static void main(String[] args){
 		BP bp = new BP(8,10,1,0.1);
 		bp.train(50);
